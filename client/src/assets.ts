167e971cb094ac251e79868e924baeea43f57910
Ernest Juchheim <juchheim@gmail.com>
2025-11-21 18:35:15 -0600

    feat(client): add Scout unit image and enable in assets

--- client/src/assets.ts
+++ client/src/assets.ts
@@ -1,14 +1,14 @@
 import SettlerImg from "../public/units/Settler.png";
 
 // Unit asset imports
-// import ScoutImg from "../public/units/Scout.png";
+import ScoutImg from "../public/units/Scout.png";
 // import SpearGuardImg from "../public/units/SpearGuard.png";
 // import WarriorImg from "../public/units/Warrior.png";
 // Add additional unit imports above as assets are created.
 
 export const unitImages: Record<string, string> = {
     Settler: SettlerImg,
-    // Scout: ScoutImg,
+    Scout: ScoutImg,
     // SpearGuard: SpearGuardImg,
     // Warrior: WarriorImg,
     // Add more unit image mappings above as they come online.
